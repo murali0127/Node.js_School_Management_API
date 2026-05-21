7320e582bcd2acbc944a368aa4232643866ffec0
murali0127 <smuralidharan845@gmail.com>
2026-05-21 13:38:29 +0530

    added cors

--- index.js
+++ index.js
@@ -10,17 +10,23 @@ require('dotenv').config();
 
 const app = express();
 
-
+//Rate-Limiter
 const limiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes window
       max: 100,                  // max 100 requests per IP for every 15 minutes
       message: { success: false, message: 'Too many requests, please try again later.' }
 })
+//Cors Protection
+const corsOption = {
+      origin: process.env.ALLOWED_ORIGIN || '*',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type']
+};
 
 
 
 app.use(helmet());                              // sets 11 secure headers
-app.use(cors());                                // allow cross-origin requests
+app.use(cors(corsOption));                                // allow cross-origin requests
 app.use(compression());                          // gzip responses
 app.use(express.json({ limit: '10kb' }));       // parse JSON body, cap size
 app.use(express.urlencoded({ extended: true }));// parse form data
